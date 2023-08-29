ae6f8115ad3a2e67bca9ba13ac0b3e590c24e4c2
osman28tr <osmantonbul2001@gmail.com>
2023-08-29 19:08:23 +0300

    Hata mesajlarının türkçeleştirilmesi işlemi gerçekleştirildi.

--- EasyCashIdentityProject.PresentationLayer/Program.cs
+++ EasyCashIdentityProject.PresentationLayer/Program.cs
@@ -1,5 +1,6 @@
 using EasyCashIdentityProject.DataAccessLayer.Concrete;
 using EasyCashIdentityProject.EntityLayer.Concrete;
+using EasyCashIdentityProject.PresentationLayer.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<Context>();
-builder.Services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<Context>();
+builder.Services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<Context>()
+.AddErrorDescriber<CustomIdentityValidator>();
 
 var app = builder.Build();
 
