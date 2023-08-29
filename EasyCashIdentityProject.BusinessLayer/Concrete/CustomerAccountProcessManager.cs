87bc2c50c3b4e59e22f7946514c49e41e38a2005
osman28tr <osmantonbul2001@gmail.com>
2023-08-29 17:31:26 +0300

    Business katmanında servis işlemleri gerçekleştirildi ve proje için tema dosyaları eklendi.

---
+++ EasyCashIdentityProject.BusinessLayer/Concrete/CustomerAccountProcessManager.cs
@@ -0,0 +1,45 @@
+﻿using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.Concrete
+{
+    public class CustomerAccountProcessManager : ICustomerAccountProcessDal
+    {
+        private readonly ICustomerAccountProcessDal _customerAccountProcessDal;
+
+        public CustomerAccountProcessManager(ICustomerAccountProcessDal customerAccountProcessDal)
+        {
+            _customerAccountProcessDal = customerAccountProcessDal;
+        }
+
+        public void Delete(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDal.Delete(t);
+        }
+
+        public List<CustomerAccountProcess> GetAll()
+        {
+            return _customerAccountProcessDal.GetAll();
+        }
+
+        public CustomerAccountProcess GetById(int id)
+        {
+            return _customerAccountProcessDal.GetById(id);
+        }
+
+        public void Insert(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDal.Insert(t);
+        }
+
+        public void Update(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDal.Update(t);
+        }
+    }
+}
