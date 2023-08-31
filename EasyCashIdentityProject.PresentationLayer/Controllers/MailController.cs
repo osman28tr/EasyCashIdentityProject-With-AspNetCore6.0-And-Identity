3c3b5a73c886214499b92d489402e7e8b6daf323
osman28tr <osmantonbul2001@gmail.com>
2023-08-31 13:09:40 +0300

    Mail onay kodu doğrulama işlemi ve bunun için bir sayfa tasarımı gerçekleştirildi.

--- EasyCashIdentityProject.PresentationLayer/Controllers/MailController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/MailController.cs
@@ -1,19 +1,33 @@
-﻿using EasyCashIdentityProject.PresentationLayer.Models;
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using EasyCashIdentityProject.PresentationLayer.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EasyCashIdentityProject.PresentationLayer.Controllers
 {
 	public class MailController : Controller
 	{
+		private readonly UserManager<AppUser> _userManager;
+		public MailController(UserManager<AppUser> userManager)
+		{
+			_userManager = userManager;
+		}
 		[HttpGet]
-		public async Task<IActionResult> Index(int id)
+		public IActionResult Index()
 		{
 			var mail = TempData["Mail"];
+			ViewBag.v = mail;
+			//confirmMailViewModel.Mail = mail.ToString();
 			return View();
 		}
 		[HttpPost]
-		public IActionResult Index(ConfirmMailViewModel confirmMailViewModel)
+		public async Task<IActionResult> Index(ConfirmMailViewModel confirmMailViewModel)
 		{
+			var user = await _userManager.FindByEmailAsync(confirmMailViewModel.Mail);
+			if(user.ConfirmCode == confirmMailViewModel.ConfirmCode)
+			{
+				return RedirectToAction("Index", "Profile");
+			}
 			return View();
 		}
 	}
