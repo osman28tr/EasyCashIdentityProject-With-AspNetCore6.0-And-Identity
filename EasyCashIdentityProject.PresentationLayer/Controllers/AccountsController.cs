80a5b0d50d743d30473b95af85820abbfe857a66
osman28tr <osmantonbul2001@gmail.com>
2023-09-06 14:23:18 +0300

    Customer layout düzenlemeler yapıldı.

---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/AccountsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
